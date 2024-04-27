e0e108d0b7c42baec6ee32af197324cd4044332b
Ivana Puškarić <ivana.puskaric2@gmail.com>
2024-04-27 12:37:32 +0200

    React-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import Navbar from "./Navbar"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import About from "./pages/About"
+import {Route, Routes } from "react-router-dom"
 
 function App() {
-    return <Navbar />
+   
+    return (
+        <>
+        <Navbar />
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pricing" element={<Pricing />} />
+            </Routes>
+
+
+        </div>
+        </>
+    )
 }
 
 export default App
